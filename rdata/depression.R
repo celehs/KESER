184f1d01a7101a85a42c071e415daa8848e0e4f9
celehs <tcai@hsph.harvard.edu>
2020-05-09 08:33:49 +0000

    2020-05-09

--- rdata/depression.R
+++ rdata/depression.R
@@ -1,15 +1,17 @@
+# Rscript depression.R > depression.txt 2>&1
+
 rm(list = ls())
 
 load("data_example_depression.rda")
 
 depression <- list(
-  cos_lst = cos_lst,
-  X_full_lst = X_full_lst, 
-  Y_full_lst = Y_full_lst,
-  X_train_lst = X_train_lst, 
-  Y_train_lst = Y_train_lst, 
-  X_valid_lst = X_valid_lst, 
-  Y_valid_lst = Y_valid_lst)
+  # cos_lst = cos_lst,
+  X_full_lst = X_full_lst[1:2], 
+  Y_full_lst = Y_full_lst[1:2],
+  X_train_lst = X_train_lst[1:2], 
+  Y_train_lst = Y_train_lst[1:2], 
+  X_valid_lst = X_valid_lst[1:2], 
+  Y_valid_lst = Y_valid_lst[1:2])
 
 str(depression)
 
