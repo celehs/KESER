562edd3e88513e3effd179830c2ce674ce227fcb
moleibobliu <lml0127@sina.com>
2020-06-24 20:33:57 -0700

    Update drop-fun.R

--- R/drop-fun.R
+++ R/drop-fun.R
@@ -1,5 +1,5 @@
 # Dropout and up-sample
-drop_fun <- function(X, Y, drop_rate = 0.5, up_rate = 2) {
+drop_fun <- function(X, Y, drop_rate = 0.5, up_rate = 3) {
   boot_ind <- sample(1:nrow(X), up_rate * nrow(X), replace = TRUE)
   Y_new <- Y[boot_ind]
   X_new <- X[boot_ind, ]
